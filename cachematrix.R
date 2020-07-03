98b7c697781e110015add6cf8f1df700e1185b2b
SIRE0726 <67798005+SIRE0726@users.noreply.github.com>
2020-07-03 21:58:33 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#So basically worked of the code given
+#The first tiny edit I made is replacing that numeric given to a 3 by 3 matrix with numbers 1 to 9 
+#Then assigned that matrix to x
+#Second, edit made was to replace all the mentions of "mean" with "solve"
+makeCacheMatrix <- function(x = matrix(1:9, nrow = 3, ncol = 3)) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
-
+#Edit made was to replace all the mentions of "mean" with "solve"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
